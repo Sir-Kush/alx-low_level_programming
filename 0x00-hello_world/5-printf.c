bdaa2c37569c826b558359470fed0f0fc6fb51f5
Sir-Kush <ikwalex23@gmail.com>
2022-09-11 05:58:15 -0700

    5-printf.c done

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+*main- An entry point that print the string in printf function
+*
+*Description: Using the main function.
+* This program prints
+*"with proper grammar, but the outcome is a piece of art,
+*Return: 0
+*/
+int main(void)
+{
+	puts("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
