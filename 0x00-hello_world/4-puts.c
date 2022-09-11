c5ca29b52539b565fb7e6f900f26613faf7416d0
Sir-Kush <ikwalex23@gmail.com>
2022-09-11 05:34:28 -0700

    4-puts.c done

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,12 +5,10 @@
 *
 *Description: Using the main function.
 * This program prints
-*Programming is like building a multilingual puzzle
+*"Programming is like building a multilingual puzzle
 *Returns0
 */
-
 int main (void)
-{
 	puts("Programming is like building a multilingual puzzle\n");
 	return 0;
 }
