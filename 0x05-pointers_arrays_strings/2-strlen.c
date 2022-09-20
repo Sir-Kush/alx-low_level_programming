bf320fa02db62e7a858114fdd075b81611179825
Sir-Kush <ikwalex23@gmail.com>
2022-09-19 18:27:10 -0700

    solution

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _strlen - a return function
+ * @s: string
+ *
+ * Return: len
+ */
+
+
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while
+	       (s[len]);
+		len++;
+	return (len);
