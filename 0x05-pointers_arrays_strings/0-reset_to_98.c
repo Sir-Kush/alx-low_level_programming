d8b8cca4cc29746de4b0778f2a0a95d5e7f29e05
Sir-Kush <ikwalex23@gmail.com>
2022-09-19 17:31:00 -0700

    solution

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,12 +1,12 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
 * reset_to_98 - set integer to 98
-* @n: a pointer to integer to bebupdated to point 98
 *
-Return: nothing
+* @n: a pointer to integer to bebupdated to point 98
+* Return: nothing
 */
+
 void reset_to_98(int *n)
 {
 	*n = 98;
