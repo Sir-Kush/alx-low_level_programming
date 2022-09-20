2bdc87d149e85931eb094f1ab5e74b6fb7b7438b
Sir-Kush <ikwalex23@gmail.com>
2022-09-19 17:55:49 -0700

    solution

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+* swap_int - Swap two integers a and b
+* @a: first integer
+* @b: second integer
+*
+* Return: nothing.
+*/
+
+void swap_int(int *a, int *b)
+{
+	int x = *a;
+	*a = *b;
+}
