9b30af54ec35ca37b5f45c8c914d7058d529e259
Sir-Kush <ikwalex23@gmail.com>
2022-09-30 18:58:21 -0700

    Solutions

---
+++ 0x09-static_libraries/3-puts.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include "stdio.h"
+/**
+*_puts - to print a string andmove to next line
+*@str: string to print
+*Return: 0
+*/
+
+void _puts(char *str)
+{
+	int i = 0;
+
+	while (str[i])
+	{
+		putchar(str[i]);
+		i++;
+	}
+	putchar('\n');
+}
